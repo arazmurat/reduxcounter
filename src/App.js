a2e4d76cffd4e4c76899c1d389ed3caccbdb2fb3
Arazmyrat <arazmirat@icloud.com>
2021-05-10 05:22:21 +0400

    add color button

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import './App.css';
 import 'semantic-ui-css/semantic.min.css'
 import Counter from './Counter'
 
+
 function App() {
   return (
     <div className="App">
-    APP JS
+    <h1> COUNTER </h1>
     <Counter/>
    </div>
   );
--- src/Counter.js
+++ src/Counter.js
@@ -1,32 +1,31 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux';
-import { increaseCount,decreaseCount } from "./actions/counterAction";
-import { Button, Icon } from 'semantic-ui-react'
+import React from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { increaseCount, decreaseCount } from "./actions/counterAction";
+import { Button, Icon } from "semantic-ui-react";
 
 export const Counter = (props) => {
-    return (
-      <div>
-        <Button onClick={props.increaseCount} icon labelPosition="right">
-          Increase
-          <Icon name="plus" />
-        </Button>
-        <span style={{padding:"10px"}}>{props.count}</span>
-        <Button onClick={props.decreaseCount} icon labelPosition="left">
-          <Icon name="minus" />
-          Decrease
-        </Button>
-      </div>
-    );
-}
+  return (
+    <div>
+      <Button onClick={props.decreaseCount} icon labelPosition="left" basic color='orange' content='Orange' >
+        <Icon name="minus" />
+        Decrease
+      </Button>
+      <span style={{ padding: "10px" }}>{props.count}</span>
+      <Button onClick={props.increaseCount} icon labelPosition="right" basic color='purple' content='Purple'>
+        Increase
+        <Icon name="plus" />
+      </Button>
+    </div>
+  );
+};
 
 const mapStateToProps = (state) => {
-    return {
-        count:state.count
-    };
-}
-const mapDispatchToProps =(dispatch)=> 
-   bindActionCreators({ increaseCount,decreaseCount},dispatch);
+  return {
+    count: state.count,
+  };
+};
+const mapDispatchToProps = (dispatch) =>
+  bindActionCreators({ increaseCount, decreaseCount }, dispatch);
 
-
-export default connect(mapStateToProps, mapDispatchToProps)(Counter)
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
